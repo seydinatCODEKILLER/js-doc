f2b0c554d38da0cbca75a51bfdec7174a239bbbe
Faty Rokhy <fatyrokhy777@gmail.com>
2025-07-25 00:37:58 +0000

    création d'une nouvelle branch feat/mme_niass/client_article

---
+++ src/services/client/ClientArticleService.js
@@ -0,0 +1,24 @@
+import { AbstractService } from "../../app/core/AbstractService.js";
+import { ClientArticle } from "../../models/ClientArticle.js";
+
+
+export class ClientArticleService extends AbstractService {
+  constructor({ api }) {
+    super({ api });
+    this.api = api;
+  }
+
+  async getAllArticles() {
+    const article = await this.api.get("/produits");
+    return article;
+  }
+
+  async getArticle(id) {
+    try {
+      return await this.api.get(`/produits/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+}
